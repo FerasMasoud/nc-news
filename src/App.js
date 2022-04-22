d7b5b29072fbd33df467e7c8bb0216844ac872ef
FerasMasoud <97962130+FerasMasoud@users.noreply.github.com>
2022-04-22 15:51:36 +0100

    Update src/App.js
    Co-authored-by: Joey Imlay <jo.imlay@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route, Routes,  } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Articles from './components/Articles';
 import Topic from './components/Topic';
 import Nav from './components/Nav';
